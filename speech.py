0d262e6e96397b6518f62d61810a8a009185e5a7
Emin <102097443+aemiink@users.noreply.github.com>
2025-02-18 23:50:15 +0300

    Update speech.py

--- speech.py
+++ speech.py
@@ -23,19 +23,16 @@ import speech_recognition as sr
 
 
 def speechlan(lang):
-    #Mikrofon değerini bir değişkenin içerisine atamak
     mic = sr.Microphone()
-    #Analog formatta ses Kaydının kaydedildiği değişken:
     recog = sr.Recognizer()
-
-    # Bir mikrofon nesnesini (mic) alarak, bu nesneyi kullanarak ses kaydı yapacağımız bir ortam oluşturur
-    # Bu sayede, kod bloğu tamamlandığında mikrofon otomatik olarak kapatılır.
     with mic as audio_file:
-        # Mikrofon üzerinden alınan ilk ses kayıtlarını kullanarak ortam gürültüsünü analiz eder. 
-        # Bu sayede, daha sonraki ses kayıtlarında ortam gürültüsünün etkisi azaltılır.
         recog.adjust_for_ambient_noise(audio_file)
-        # Analog Formattan Sesin Dijital Ortama Aktarılması işidir.
-        audio = recog.listen(audio_file)
-        # Satırı, alınan ses kaydını Google'ın ses tanıma API'sini kullanarak metin formatına çevirir.
-        return recog.recognize_google(audio, language=lang)
+        try:
+            audio = recog.listen(audio_file, timeout=5)  # Maksimum 5 saniye bekle
+            return recog.recognize_google(audio, language=lang)
+        except sr.UnknownValueError:
+            return "Anlaşılamadı, lütfen tekrar deneyin."
+        except sr.RequestError:
+            return "Bağlantı hatası, lütfen internetinizi kontrol edin."
+
 
